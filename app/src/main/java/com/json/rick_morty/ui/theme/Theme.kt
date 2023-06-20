cf256317c1edc39558bce750188864aa9f9bcd31
Jeison <jeisondh2311@gmail.com>
2023-06-20 10:49:26 -0600

    Fixed theme packages

--- app/src/main/java/com/json/rick_morty/ui/theme/Theme.kt
+++ app/src/main/java/com/json/rick_morty/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.compose
+package com.json.rick_morty.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -73,7 +73,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
+fun RickMortyTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
