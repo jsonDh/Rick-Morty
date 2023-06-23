b1b80cbd2a743c44fcc7bd5f1f3f5083e65fce33
Jeison <jeisondh2311@gmail.com>
2023-06-23 10:32:19 -0600

    Changing from Activity to fragment

--- app/src/main/java/com/json/rick_morty/presentation/view/fragments/CharacterListFragment.kt
+++ app/src/main/java/com/json/rick_morty/presentation/view/fragments/CharacterListFragment.kt
@@ -1,9 +1,9 @@
-package com.json.rick_morty.presentation.view.activities
+package com.json.rick_morty.presentation.view.fragments
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,31 +11,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.json.rick_morty.presentation.view.components.ShowCharactersList
 import com.json.rick_morty.presentation.view.components.SmallAppBar
 import com.json.rick_morty.presentation.view.ui.theme.RickMortyTheme
 import com.json.rick_morty.presentation.viewmodel.CharactersListState
 import com.json.rick_morty.presentation.viewmodel.CharactersViewModel
-import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
-@AndroidEntryPoint
-class CharactersListActivity : ComponentActivity() {
+class CharacterListFragment : Fragment() {
 
-    private val charactersViewModel: CharactersViewModel by viewModels()
+    private val listViewModel : CharactersViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            RickMortyTheme {
-                CharacterListScreen(charactersViewModel)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        lifecycleScope.launch {
+            listViewModel.charactersListState.collect { charactersState ->
+                if (charactersState is CharactersListState.Initial) {
+                    listViewModel.getCharacters()
+                }
             }
         }
-        lifecycleScope.launch {
-            charactersViewModel.charactersListState.collect { charactersState ->
-                if (charactersState is CharactersListState.Initial) {
-                    charactersViewModel.getCharacters()
+        return ComposeView(requireContext()).apply {
+            setContent {
+                RickMortyTheme {
+                    CharacterListScreen(listViewModel)
                 }
             }
         }
@@ -56,5 +62,3 @@ fun CharacterListScreen(charactersViewModel: CharactersViewModel) {
         }
     )
 }
-
-
