bbc4b66362dd45a17ecdc78a57b1fa7f2bd8fcd1
Jeison <jeisondh2311@gmail.com>
2023-06-22 23:41:03 -0600

    Adding fragment, views, viewmodel, and resources needed for the Details View Screen

--- app/src/main/java/com/json/rick_morty/presentation/view/components/GeneralViewComponents.kt
+++ app/src/main/java/com/json/rick_morty/presentation/view/components/GeneralViewComponents.kt
@@ -2,10 +2,16 @@ package com.json.rick_morty.presentation.view.components
 
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.json.rick_morty.R
 
 
 @Composable
@@ -14,6 +20,32 @@ fun ShowLoading() {
         modifier = Modifier.fillMaxWidth(),
         color = MaterialTheme.colorScheme.primary,
         trackColor = MaterialTheme.colorScheme.onBackground,
+    )
+}
+
+@Composable
+fun LargeAppBar(title : String){
+    LargeTopAppBar(
+        title = {
+            Text(text = title)
+        },
+        colors = TopAppBarDefaults.largeTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
+}
+
+@Composable
+fun SmallAppBar(){
+    TopAppBar(
+        title = {
+            Text(stringResource(id = R.string.app_name))
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        )
     )
 }
 
